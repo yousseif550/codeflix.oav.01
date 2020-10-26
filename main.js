4f582a1ab4911cf96591e4bef348a2f77da45212
Yousseif <yousseif550@gmail.com>
2020-10-26 17:44:54 +0300

    oav.01

---
+++ main.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+let regex = /.*:.*/gm;
+let configFile = 'php.json'  
+
+let fileContent = fs.readFileSync('env').toString()
+let found = fileContent.match(regex)
+
+  fs.open(configFile, 'w+', function (err, f) {
+    if (err) {
+        return console.error(err); 
+    }
+  })
+
+  fs.writeFile(configFile, JSON.stringify(found, null, '\t'),(err) => {
+    if (err) throw err;
+  })
+
+
+
+
+
